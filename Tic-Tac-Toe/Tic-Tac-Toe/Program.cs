ffec16504dfbf679ccfeaf825f4469f8e33fae4f
diwashrestha <diwash712@gmail.com>
2020-06-25 14:49:31 +0545

    Add Tic Tac Toe

---
+++ Tic-Tac-Toe/Tic-Tac-Toe/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Tic_Tac_Toe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("X O X");
+            Console.WriteLine("O X O");
+            Console.WriteLine("X X O");
+        }
+    }
+}
