1c9172ef7d20f9b9fb7c27a6123be3d92661bc5c
diwashrestha <diwash712@gmail.com>
2020-06-25 20:46:20 +0545

    uSER IS THE GAMEMASTER

--- Tic-Tac-Toe/Tic-Tac-Toe/Program.cs
+++ Tic-Tac-Toe/Tic-Tac-Toe/Program.cs
@@ -6,9 +6,46 @@ namespace Tic_Tac_Toe
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("X O X");
-            Console.WriteLine("O X O");
-            Console.WriteLine("X X O");
+            // Console.WriteLine("X O X");
+            // Console.WriteLine("O X O");
+            // Console.WriteLine("X X O");
+
+            
+            Console.WriteLine("Enter cells:");
+            string userInput = Console.ReadLine();
+            Console.WriteLine(userInput);
+            char[] user1dInput = userInput.ToCharArray();
+
+            char[,] user2dInput = new char[3,3];
+            int index = 0;
+
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    user2dInput[i, j] = user1dInput[index];
+                    index++;
+                }
+            }
+
+            Console.WriteLine("---------");
+            for (int i = 0; i < 3; i++)
+            {
+                Console.Write("|");
+                Console.Write(" ");
+                for (int j = 0; j < 3; j++)
+                {
+                    Console.Write(user2dInput[i,j]);
+                    Console.Write(" ");
+                }
+                Console.Write(" ");
+                Console.Write("|");
+                Console.Write("\n");
+            }
+            Console.WriteLine("---------");
+
+
+            Console.ReadLine();
         }
     }
 }
