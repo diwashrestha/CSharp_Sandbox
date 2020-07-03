755163f6b915e1de6a035d397c0799a580af146f
diwashrestha <diwash712@gmail.com>
2020-07-03 12:24:04 +0545

    Stage 3/5  What's up on the field?

--- Tic-Tac-Toe/Tic-Tac-Toe/Program.cs
+++ Tic-Tac-Toe/Tic-Tac-Toe/Program.cs
@@ -6,16 +6,20 @@ namespace Tic_Tac_Toe
     {
         public static void Main(string[] args)
         {
+            // stage 1
             // Console.WriteLine("X O X");
             // Console.WriteLine("O X O");
             // Console.WriteLine("X X O");
 
 
+            // stage 2
             Console.WriteLine("Enter cells:");
             string userInput = Console.ReadLine();
             Console.WriteLine(userInput);
             char[] user1dInput = userInput.ToCharArray();
 
+            bool gameEnd = false;
+            string gameMessage = "";
             char[,] user2dInput = new char[3,3];
             int index = 0;
 
@@ -44,54 +48,107 @@ namespace Tic_Tac_Toe
             }
             Console.WriteLine("---------");
 
-            GameCases(user2dInput);
-            CheckWin(user2dInput);
+
+            if (CheckWin(user2dInput, ref gameEnd, ref gameMessage))
+            {
+                Console.WriteLine("{0}",gameMessage);
+            }
             Console.ReadLine();
         }
 
-        public static void GameCases(char[,] userInput)
-        {
-            string gameResult = "";
-            var gameSign = new[] {'X', 'O', '_'};
 
-            for (int i = 0; i < 3; i++)
+
+        private static bool CheckWin(char[,] input, ref bool gameEnd , ref string gameMessage)
+        {
+            // var to check the difference between 'X' and 'O' count in grid
+            int charCountDiff = 0;
+            charCountDiff = CharCounter(input, 'X') - CharCounter(input, 'O');
+
+            if ((IsWin(input,'X') && IsWin(input,'O'))|| Math.Abs(charCountDiff) >= 2)
             {
-                for (int j = 0; j < 3; j++)
-                {
-                    if ((userInput[i,j] == gameSign[2]))
-                    {
-                        gameResult = "Game not finished";
-                    }
-                }
+                gameEnd = true;
+                gameMessage = "Impossible";
             }
-            Console.WriteLine(gameResult);
-            return;
-        }
-
-
-        private static bool CheckWin(char[,] input)
-        {
-            if (IsWin(input,'X'))
+            else if (IsWin(input,'X'))
             {
-                Console.WriteLine("X WINS!");
-                
+                gameEnd = true;
+                gameMessage = "X Wins!";
             }
             else if (IsWin(input,'O'))
             {
-                Console.WriteLine("O WINS!");
+                gameEnd = true;
+                gameMessage = "O Wins!";
             }
-            return true;
+            else if (IsDraw(input))
+            {
+                gameEnd = true;
+                gameMessage = "Draw!";
+            }
+            else if (!IsWin(input, 'X') && !IsWin(input, 'O') && !IsDraw(input))
+            {
+                gameEnd = false;
+                gameMessage = " Game not finished";
+            }
 
+            return gameEnd;
         }
 
-
+        // method for wining condition
         static bool IsWin(char[,] userInput, char sign)
         {
             // row win case
             return (userInput[0, 0] == sign && userInput[0, 1] == sign && userInput[0, 2] == sign
                     || userInput[1, 0] == sign && userInput[1, 1] == sign && userInput[1, 2] == sign
-                    || userInput[2, 0] == sign && userInput[2, 1] == sign && userInput[2, 2] == sign);
+                    || userInput[2, 0] == sign && userInput[2, 1] == sign && userInput[2, 2] == sign
+                    // column win case
+                    || userInput[0, 0] == sign && userInput[1, 0] == sign && userInput[2, 0] == sign
+                    || userInput[0, 1] == sign && userInput[1, 1] == sign && userInput[2, 1] == sign
+                    || userInput[0, 2] == sign && userInput[1, 2] == sign && userInput[2, 2] == sign
+                    // diagonal win case
+                    || userInput[0, 0] == sign && userInput[1, 1] == sign && userInput[2, 2] == sign
+                    || userInput[0, 2] == sign && userInput[1, 1] == sign && userInput[2, 0] == sign);
+        }
 
+        // method to check the draw condition
+        static bool IsDraw(char[,] userInput)
+        {
+            // var for the space in the grid
+            bool isSpace = false;
+            // var to find is there draw in the game or not
+            bool isDraw = false;
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    if ((userInput[i, j] == '_'))
+                    {
+                        isSpace = true;
+                    }
+                }
+            }
+
+            if (isSpace == false && !IsWin(userInput, 'X') && !IsWin(userInput, 'O'))
+            {
+                isDraw = true;
+            }
+
+            return isDraw;
+        }
+
+        static int CharCounter(char[,] userArray, char sign)
+        {
+            int count = 0;
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    if ((userArray[i, j] == sign))
+                    {
+                        count++;
+                    }
+                }
+            }
+            return count;
         }
     }
 }
